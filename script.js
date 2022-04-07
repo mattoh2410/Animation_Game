ec05ecfe825035c60b4751a5b5ffff9658bc35bf
mattoh2410 <63155445+mattoh2410@users.noreply.github.com>
2022-04-07 01:25:01 +0100

    Add files via upload
    Animation kinda working need to work on what is passed to the inline css

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ var animation_speed=1;
 var test ;
 let gameovervoice = new Audio();
 gameovervoice.src = 'gameover1.wav';
+let plus5=5;
 
 let jumpsound = new Audio();
 jumpsound.src = 'jumpsound.mp3';
@@ -46,8 +47,6 @@ var checkDead = setInterval(function(){
         parseInt(window.getComputedStyle(character). getPropertyValue("top"));   
     var blockLeft =    
         parseInt(window.getComputedStyle(block). getPropertyValue("left"));
-        document.getElementById("block").style.animation = 'block '+animation_speed+'s linear'; 
-
     if(blockLeft<20 && blockLeft>0 && characterTop>=160){ 
         blocksound.pause;
         blocksound.src = "";
@@ -65,13 +64,13 @@ var checkDead = setInterval(function(){
         gameovervoice.play();
     }else{
         score++;
-        if(animation_count=Math.floor(Math.floor(score/100 % 10))== 5) {
-           animation_speed = animation_speed +0.1
-           console.log("block"+animation_speed+"s linear");
-        
+        if(score/100 ==5 || score/100==plus5) {
+           animation_speed+=1
+            document.getElementById("block").style = 'animation: block '+animation_speed+'s linear; animation-iteration-count: infinite; position: relative;;'
+           plus5+=5; 
         }
+        
         document.getElementById("scoreSpan").innerHTML = Math.floor(score/100);
-        console.log(animation_speed);
 }
 },10);
 
